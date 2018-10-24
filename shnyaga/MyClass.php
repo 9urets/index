ce6e92d5ca99a16a56da1485a49f5b99e64db110
b0mb0m <yurij.radchuk@mail.ru>
2018-10-24 21:39:31 +0300

    first commit

---
+++ shnyaga/MyClass.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Юрий
+ * Date: 24.10.2018
+ * Time: 21:25
+ */
+
+class MyClass
+{
+    public function test(){
+        echo 'добавляем файл на git';
+    }
+}
